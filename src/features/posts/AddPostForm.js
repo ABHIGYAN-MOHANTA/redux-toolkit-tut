2409004e6812cd341ae02c0ff0a885986d83f7af
Abhigyan Mohanta <110360901+ABHIGYAN-MOHANTA@users.noreply.github.com>
2023-06-10 15:21:38 +0530

    createAsyncThunk

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,13 +1,16 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-import { postAdded } from "./postsSlice";
+import { addNewPost } from "./postsSlice";
 import { selectAllUsers } from "../users/usersSlice";
 
 const AddPostForm = () => {
+  const dispatch = useDispatch();
+
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [userId, setUserId] = useState("");
+  const [addRequestStatus, setAddRequestStatus] = useState("idle");
 
   const users = useSelector(selectAllUsers);
 
@@ -15,32 +18,35 @@ const AddPostForm = () => {
   const onContentChanged = (e) => setContent(e.target.value);
   const onAuthorChanged = (e) => setUserId(e.target.value);
 
-  const dispatch = useDispatch();
+  const canSave =
+    [title, content, userId].every(Boolean) && addRequestStatus === "idle";
 
   const onSavePostClicked = () => {
-    if (title && content) {
-      dispatch(
-        postAdded(title, content, userId)
-        //   {id: nanoid(),
-        //   title,
-        //   content,}
-      );
+    if (canSave) {
+      try {
+        setAddRequestStatus("pending");
+        dispatch(addNewPost({ title, body: content, userId })).unwrap();
 
-      setTitle("");
-      setContent("");
+        setTitle("");
+        setContent("");
+        setUserId("");
+      } catch (err) {
+        console.error("Failed to save the post", err);
+      } finally {
+        setAddRequestStatus("idle");
+      }
     }
   };
-
-  const canSave = Boolean(title) && Boolean(content) && Boolean(userId);
 
   const usersOptions = users.map((user) => (
     <option key={user.id} value={user.id}>
       {user.name}
     </option>
   ));
+
   return (
     <section>
-      <h2>Add a new Form</h2>
+      <h2>Add a New Post</h2>
       <form>
         <label htmlFor="postTitle">Post Title:</label>
         <input
@@ -55,13 +61,13 @@ const AddPostForm = () => {
           <option value=""></option>
           {usersOptions}
         </select>
-        <label htmlFor="postContent">Content: </label>
+        <label htmlFor="postContent">Content:</label>
         <textarea
           id="postContent"
           name="postContent"
           value={content}
           onChange={onContentChanged}
-        ></textarea>
+        />
         <button type="button" onClick={onSavePostClicked} disabled={!canSave}>
           Save Post
         </button>
